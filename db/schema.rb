8d9bd61767af942eab5d9ae601d11e51e595c478
ihojmanb <ihojmanb@gmail.com>
2021-11-24 14:46:47 -0300

    add balance column in player table with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_000818) do
+ActiveRecord::Schema.define(version: 2021_11_24_173124) do
 
   create_table "players", force: :cascade do |t|
     t.string "first_name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_23_000818) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "balance", default: 10000
     t.index ["email"], name: "index_players_on_email", unique: true
   end
 
