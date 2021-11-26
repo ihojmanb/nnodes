8d88c0f226d550c05336fe7572d3ab008aa19537
ihojmanb <ihojmanb@gmail.com>
2021-11-26 11:31:13 -0300

    fix dashboard and cron-jobs

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,21 @@ Player.create!(first_name: "Ianiv",
                  last_name: last_name,
                  email: email)
 end
-
+case Rails.env
+when "development"
+  5.times do
+    @roulette = Roulette.create
+    @roulette.result = @roulette.get_bet_color
+    @roulette.create_round
+    players = Player.order("created_at DESC")
+    players.each do |player|
+      bet_amount = player.get_bet_amount()
+      bet_color = player.get_bet_color()
+      bet = player.bets.create!(amount: bet_amount, color: bet_color, round: @roulette.get_actual_round)
+      player.bet(bet)
+    end
+  end
+end
 # @roulette = Roulette.create
 # @roulette.result = @roulette.get_bet_color
 # @roulette.create_round
